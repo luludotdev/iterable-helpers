b025d6cbae30dd3a9de1aeeb9a5b3d16820b300d
Jack Baron <jackmbaron@gmail.com>
2019-05-06 06:52:34 +0100

    limiter

---
+++ src/limit.ts
@@ -0,0 +1,23 @@
+export const limit: <T>(
+  iter: IterableIterator<T>,
+  upper: number
+) => IterableIterator<T> = function*(iter, upper) {
+  for (let i = 0; i < upper; i++) {
+    const next = iter.next()
+
+    if (next.done) break
+    else yield next.value
+  }
+}
+
+export const limitAsync: <T>(
+  iter: AsyncIterableIterator<T>,
+  upper: number
+) => AsyncIterableIterator<T> = async function*(iter, upper) {
+  for (let i = 0; i < upper; i++) {
+    const next = await iter.next()
+
+    if (next.done) break
+    else yield next.value
+  }
+}
