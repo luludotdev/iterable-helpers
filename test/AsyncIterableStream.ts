fc690714947da3e829051b5321ee8148d6086815
Jack Baron <jackmbaron@gmail.com>
2019-05-06 07:42:29 +0100

    check for stream inheritence

--- test/AsyncIterableStream.ts
+++ test/AsyncIterableStream.ts
@@ -1,6 +1,11 @@
 import test from 'ava'
+import { Readable } from 'stream'
 import { AsyncIterableStream } from '../src'
 
 test('is a function', t => {
   return t.is(typeof AsyncIterableStream, 'function')
+})
+
+test('is a readable stream', t => {
+  return t.is(AsyncIterableStream.prototype instanceof Readable, true)
 })
--- test/IterableStream.ts
+++ test/IterableStream.ts
@@ -1,6 +1,11 @@
 import test from 'ava'
+import { Readable } from 'stream'
 import { IterableStream } from '../src'
 
 test('is a function', t => {
   return t.is(typeof IterableStream, 'function')
+})
+
+test('is a readable stream', t => {
+  return t.is(IterableStream.prototype instanceof Readable, true)
 })
